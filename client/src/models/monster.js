85e236e7ae55a1f60a307e9a75ae996e8ed31845
Valerie Gibson <vgibson14@gmail.com>
2016-07-24 17:43:08 +0100

    adds monster methods to prototype

--- client/src/models/monster.js
+++ client/src/models/monster.js
@@ -1,17 +1,19 @@
 var Monster = function(worldDimensions) {
   this.worldDimensions = worldDimensions
-  this.imageSize = 3
-
-  this.randomPos = function(position) {
-    return this.imageSize + (Math.random() * (position - (this.imageSize * 2)))
-  }
-
-  this.setPosition = function(){
-    this.x = this.randomPos(worldDimensions.width)
-    this.y = this.randomPos(worldDimensions.height)
-  }
-
+  this.imageSize = 32
   this.setPosition()
  
 }
+
+Monster.prototype = {
+  randomPos: function(position) {
+    return this.imageSize + (Math.random() * (position - (this.imageSize * 2)))
+  },
+  setPosition: function(){
+    this.x = this.randomPos(this.worldDimensions.width)
+    this.y = this.randomPos(this.worldDimensions.height)
+  }
+
+}
+
 module.exports = Monster;
