d8ccb83adb4c70f5451e287b297f461826d0394e
Valerie Gibson <vgibson14@gmail.com>
2016-07-29 18:41:23 +0100

    stop chicken spawning in walls etc:

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -125,12 +125,21 @@
 	}
 	
 	Monster.prototype = {
-	  randomPos: function(position) {
-	    return this.imageSize + (Math.random() * (position - (this.imageSize * 2)))
+	  //this doesn't belong in here
+	  randomPos: function(dimension) {
+	    var smallest = this.imageSize * 2;
+	    var largest = dimension - this.imageSize;
+	    return this.randomIntFromInterval(smallest, largest);
+	    
 	  },
 	  setPosition: function(){
 	    this.x = this.randomPos(this.worldDimensions.width)
 	    this.y = this.randomPos(this.worldDimensions.height)
+	    console.log("x: ",this.x)
+	    console.log("y: ",this.y)
+	  },
+	  randomIntFromInterval: function(min,max){
+	    return Math.floor(Math.random()*(max-min+1)+min);
 	  }
 	
 	}
